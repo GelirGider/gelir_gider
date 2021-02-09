e787b9a732a12a771bbbc66fad366c0a8fb1d578
Ömer <omerozturkunmaili@gmail.com>
2021-02-09 17:18:14 +0300

    Repush

--- lib/widgets/expense_item.dart
+++ lib/widgets/expense_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import '../providers/expense_provider.dart';
 
@@ -9,18 +8,18 @@ class ExpenseItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Container(
-        margin: const EdgeInsets.all(15.0),
-        padding: const EdgeInsets.all(3.0),
-        decoration: BoxDecoration(border: Border.all(color: Theme.of(context).accentColor)),
-
-      title: Text(expense.description),
-      subtitle: Text(expense.category),
-      trailing: expense.isExpense
-          ? Text("- ₺ " + expense.price.toString(),
-              style: TextStyle(color: Colors.red,fontSize: 15))
-          : Text("+ ₺ " + expense.price.toString(),
-              style: TextStyle(color: Colors.green,fontSize: 15)),
-    );
+        title: Text(expense.description),
+        subtitle: Text(expense.category),
+        trailing: expense.isExpense
+            ? Text("- ₺ " + expense.price.toString(),
+                style: TextStyle(color: Colors.red, fontSize: 15))
+            : Text("+ ₺ " + expense.price.toString(),
+                style: TextStyle(color: Colors.green, fontSize: 15)),
+        leading: Container(
+          margin: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(3.0),
+          decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).accentColor)),
+        ));
   }
 }
