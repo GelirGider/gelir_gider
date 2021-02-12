9ffe8810e31b3bf302c2c8c1f6bded1de998636a
yusuf erdogan <a.erdogan2018@gtu.edu.tr>
2021-02-12 22:46:41 +0300

    todo

--- lib/screens/adding_expense_screen.dart
+++ lib/screens/adding_expense_screen.dart
@@ -264,6 +264,9 @@ class _AddingExpenseState extends State<AddingExpense> {
                                   val = Jiffy(val).format('dd/MM/yyyy');
                                   time = val;
                                 },
+                                style: TextStyle(
+                                    //TODO : burayla oyna
+                                    ),
                               ),
                             ),
                             Divider(),
