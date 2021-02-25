b28148efec786d5732996b97829bdb9483b826ec
ozkayanurhuda <ozkayanurhuda@gmail.com>
2021-02-26 01:32:46 +0300

    TabBarView and List of DMY are added

--- lib/screens/expenses_list_screen.dart
+++ lib/screens/expenses_list_screen.dart
@@ -21,6 +21,281 @@ class ExpensesListScreen extends StatefulWidget {
   _ExpensesListScreenState createState() => _ExpensesListScreenState();
 }
 
+List iconsYear = [
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '2019',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '2020',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '2021',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+];
+
+List iconsMonth = [
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'January',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'February',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'March',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'April',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'May',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'June',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'July',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'August',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'September',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'October',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'November',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      'December',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+];
+
+List iconsWeek = [
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '1',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '2',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '3',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  FlatButton(
+    onPressed: () {},
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(vertical: 30),
+    color: Colors.pink,
+    child: Text(
+      '4',
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+];
+
 class _ExpensesListScreenState extends State<ExpensesListScreen> {
   var languageIndex;
 
@@ -134,7 +409,7 @@ class _ExpensesListScreenState extends State<ExpensesListScreen> {
                 print(index);
                 setState(() {});
               },
-              labelStyle: TextStyle(fontSize: 10),
+              labelStyle: TextStyle(fontSize: 12),
               tabs: <Widget>[
                 Tab(text: S.of(context).TabBarDay),
                 Tab(text: S.of(context).TabBarWeek),
@@ -143,86 +418,152 @@ class _ExpensesListScreenState extends State<ExpensesListScreen> {
               ],
             ),
           ),
-          body: FutureBuilder(
-            future: Provider.of<Expenses>(context, listen: false)
-                .fetchAndSetExpenses(),
-            builder: (ctx, snapshot) => snapshot.connectionState ==
-                    ConnectionState.waiting
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : expenseProvider.currentItems.isEmpty
-                    ? Center(
-                        child: Text(
-                          S.of(context).ExpenseListNoneExpense,
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    : Column(
-                        children: [
-                          SizedBox(
-                            height: 15,
-                          ),
-                          MoneyWidget(
-                            income: expenseProvider
-                                .calculateTotalIncome()
-                                .toStringAsFixed(1),
-                            money: expenseProvider
-                                .calculateTotalMoney()
-                                .toStringAsFixed(1),
-                            expense: expenseProvider
-                                .calculateTotalExpense()
-                                .toStringAsFixed(1),
-                            percentage: expenseProvider.getPercentage,
-                          ),
-                          Divider(
-                            height: 25,
-                            color: _theme.getTheme() == _theme.dark
-                                ? Color.fromRGBO(1223, 81, 83, 1)
-                                : Colors.black,
-                          ),
-                          Flexible(
-                            flex: 10,
-                            child: Consumer<Expenses>(
-                              builder: (context, expenseProvider, child) {
-                                return ListView.builder(
-                                  itemCount:
-                                      expenseProvider.currentItems.length,
-                                  itemBuilder: (context, index) {
-                                    var category = expenseProvider
-                                        .currentItems.keys
-                                        .toList()[index];
-                                    var list = expenseProvider
-                                        .currentItems.values
-                                        .toList()[index];
-                                    var currency = expenseProvider.symbol;
-                                    return Column(
-                                      children: [
-                                        MainPageCategoryModal(
-                                          category: category,
-                                          list: list,
-                                          currency: currency,
-                                        ),
-                                        Divider(
-                                          height: 25,
-                                          color: _theme.getTheme() ==
-                                                  _theme.dark
-                                              ? Color.fromRGBO(1223, 81, 83, 1)
-                                              : Colors.black,
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
+          body: Container(
+            child: TabBarView(
+              children: [
+                FutureBuilder(
+                  future: Provider.of<Expenses>(context, listen: false)
+                      .fetchAndSetExpenses(),
+                  builder: (ctx, snapshot) => snapshot.connectionState ==
+                          ConnectionState.waiting
+                      ? Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : expenseProvider.currentItems.isEmpty
+                          ? Center(
+                              child: Text(
+                                S.of(context).ExpenseListNoneExpense,
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          : Column(
+                              children: [
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                MoneyWidget(
+                                  income: expenseProvider
+                                      .calculateTotalIncome()
+                                      .toStringAsFixed(1),
+                                  money: expenseProvider
+                                      .calculateTotalMoney()
+                                      .toStringAsFixed(1),
+                                  expense: expenseProvider
+                                      .calculateTotalExpense()
+                                      .toStringAsFixed(1),
+                                  percentage: expenseProvider.getPercentage,
+                                ),
+                                Divider(
+                                  height: 25,
+                                  color: _theme.getTheme() == _theme.dark
+                                      ? Color.fromRGBO(1223, 81, 83, 1)
+                                      : Colors.black,
+                                ),
+                                Flexible(
+                                  flex: 10,
+                                  child: Consumer<Expenses>(
+                                    builder: (context, expenseProvider, child) {
+                                      return ListView.builder(
+                                        itemCount:
+                                            expenseProvider.currentItems.length,
+                                        itemBuilder: (context, index) {
+                                          var category = expenseProvider
+                                              .currentItems.keys
+                                              .toList()[index];
+                                          var list = expenseProvider
+                                              .currentItems.values
+                                              .toList()[index];
+                                          var currency = expenseProvider.symbol;
+                                          return Column(
+                                            children: [
+                                              MainPageCategoryModal(
+                                                category: category,
+                                                list: list,
+                                                currency: currency,
+                                              ),
+                                              Divider(
+                                                height: 25,
+                                                color: _theme.getTheme() ==
+                                                        _theme.dark
+                                                    ? Color.fromRGBO(
+                                                        1223, 81, 83, 1)
+                                                    : Colors.black,
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      );
+                                    },
+                                  ),
+                                ),
+                                Flexible(
+                                  flex: 1,
+                                  child: Container(),
+                                )
+                              ],
                             ),
-                          ),
-                          Flexible(
-                            flex: 1,
-                            child: Container(),
-                          )
-                        ],
-                      ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    mainAxisSpacing: 0.0,
+                    crossAxisSpacing: 0.0,
+                    childAspectRatio: 7 / 5,
+                    padding: EdgeInsets.all(10.0),
+                    crossAxisCount: 2,
+                    children: List.generate(
+                      iconsWeek.length,
+                      (index) {
+                        return FlatButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacement(MaterialPageRoute(
+                              builder: (ctx) => ExpensesListScreen(),
+                            ));
+                          },
+                          child: iconsWeek[index],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    mainAxisSpacing: 0.0,
+                    crossAxisSpacing: 0.0,
+                    childAspectRatio: 1.0,
+                    padding: EdgeInsets.all(10.0),
+                    crossAxisCount: 3,
+                    children: List.generate(
+                      iconsMonth.length,
+                      (index) {
+                        return FlatButton(
+                          onPressed: () {},
+                          child: iconsMonth[index],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    mainAxisSpacing: 0.0,
+                    crossAxisSpacing: 0.0,
+                    childAspectRatio: 1.0,
+                    padding: EdgeInsets.all(10.0),
+                    crossAxisCount: 3,
+                    children: List.generate(
+                      iconsYear.length,
+                      (index) {
+                        return FlatButton(
+                          onPressed: () {},
+                          child: iconsYear[index],
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
           floatingActionButton: FloatingActionButton(
