aa43088ab9178235bd0fbac843d9c3a86e11f244
ozkayanurhuda <ozkayanurhuda@gmail.com>
2021-02-28 20:02:36 +0300

    Week List & Item added

--- lib/widgets/year_page/week_item.dart
+++ lib/widgets/year_page/week_item.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:gelir_gider/providers/expense_provider.dart';
+import 'package:gelir_gider/themes/colours.dart';
+import 'package:provider/provider.dart';
+import 'day_list_page.dart';
 
-class WeekItem extends StatelessWidget {
+class WeekListItem extends StatelessWidget {
+  final String title;
+  final int index;
+
+  const WeekListItem({Key key, this.title, this.index}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final expenseProvider = Provider.of<Expenses>(context, listen: false);
+    return FlatButton(
+      onPressed: () {
+        expenseProvider.setSelectedWeek(index);
+        expenseProvider.setSelectedPage(1);
+      },
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      color: Colours.pink,
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colours.white,
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/year_page/week_list_page.dart
+++ lib/widgets/year_page/week_list_page.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:gelir_gider/widgets/year_page/week_item.dart';
+import 'package:gelir_gider/generated/l10n.dart';
+import 'week_item.dart';
 
 class WeekListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final weekList = <String> [
+      S.of(context).Monday,
+      S.of(context).Tuesday,
+      S.of(context).Wednesday,
+      S.of(context).Thursday,
+      S.of(context).Friday,
+      S.of(context).Saturday,
+      S.of(context).Sunday,
+    ];
+
+    var weekButtons = [
+      WeekListItem(title: weekList[0],index:0),
+      WeekListItem(title: weekList[1],index:1),
+      WeekListItem(title: weekList[2],index:2),
+      WeekListItem(title: weekList[3],index:3),
+      WeekListItem(title: weekList[4],index:4),
+      WeekListItem(title: weekList[5],index:5),
+      WeekListItem(title: weekList[6],index:6),
+    ];
+    return GridView.count(
+      mainAxisSpacing: 10.0,
+      crossAxisSpacing: 10.0,
+      childAspectRatio: 1.0,
+      padding: EdgeInsets.all(30.0),
+      crossAxisCount: 3,
+      children: weekButtons,
+    );
   }
 }
